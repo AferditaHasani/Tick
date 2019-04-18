4b1374330a0682cab50960f710dfc893f0c37f68
Kaltrina99 <36515858+Kaltrina99@users.noreply.github.com>
2019-04-18 14:39:50 +0200

    Add Resources

---
+++ Tick/ExpensesManagment/ExpensesTracking.Designer.cs
@@ -0,0 +1,76 @@
+﻿namespace Tick.ExpensesManagment
+{
+    partial class ExpensesTracking
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.bunifuGradientPanel1 = new Bunifu.Framework.UI.BunifuGradientPanel();
+            ((System.ComponentModel.ISupportInitialize)(this)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // bunifuGradientPanel1
+            // 
+            this.bunifuGradientPanel1.BackgroundImageLayout = System.Windows.Forms.ImageLayout.Stretch;
+            this.bunifuGradientPanel1.ForeColor = System.Drawing.Color.FromArgb(((int)(((byte)(161)))), ((int)(((byte)(161)))), ((int)(((byte)(161)))));
+            this.bunifuGradientPanel1.GradientBottomLeft = System.Drawing.Color.Transparent;
+            this.bunifuGradientPanel1.GradientBottomRight = System.Drawing.Color.Transparent;
+            this.bunifuGradientPanel1.GradientTopLeft = System.Drawing.Color.Transparent;
+            this.bunifuGradientPanel1.GradientTopRight = System.Drawing.Color.Transparent;
+            this.bunifuGradientPanel1.Location = new System.Drawing.Point(4, 121);
+            this.bunifuGradientPanel1.Name = "bunifuGradientPanel1";
+            this.bunifuGradientPanel1.Quality = 10;
+            this.bunifuGradientPanel1.Size = new System.Drawing.Size(631, 499);
+            this.bunifuGradientPanel1.TabIndex = 0;
+            // 
+            // ExpensesTracking
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(44)))), ((int)(((byte)(42)))), ((int)(((byte)(50)))));
+            this.ClientSize = new System.Drawing.Size(640, 697);
+            this.Controls.Add(this.bunifuGradientPanel1);
+            this.Font = new System.Drawing.Font("Segoe UI Semibold", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.ForeColor = System.Drawing.Color.FromArgb(((int)(((byte)(161)))), ((int)(((byte)(161)))), ((int)(((byte)(161)))));
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.None;
+            this.Name = "ExpensesTracking";
+            // 
+            // 
+            // 
+            this.RootElement.ApplyShapeToControl = true;
+            this.ShowIcon = false;
+            this.ShowInTaskbar = false;
+            this.Text = "ExpensesTracking";
+            ((System.ComponentModel.ISupportInitialize)(this)).EndInit();
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private Bunifu.Framework.UI.BunifuGradientPanel bunifuGradientPanel1;
+    }
+}
