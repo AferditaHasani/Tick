4f04a09b8ad8782a3a85bf8fc515d9cc2140ed21
dita9488 <dita9488@gmail.com>
2019-04-18 14:28:23 +0200

    Shtimi i Time Tracking Formes dhe editimi i menu formes

--- Tick/TimeManagement/TimeTracking.cs
+++ Tick/TimeManagement/TimeTracking.cs
@@ -15,5 +15,21 @@ namespace Tick.TimeManagement
         {
             InitializeComponent();
         }
+
+        private void btnAddTimeTracking_Click(object sender, EventArgs e)
+        {
+            string[] row = new string[] {  "Product 1", "1000" };
+            dgvWorkUnits.Rows.Add(row);
+            row = new string[] { "Product 2", "2000" };
+            dgvWorkUnits.Rows.Add(row);
+            row = new string[] { "Product 3", "3000" };
+            dgvWorkUnits.Rows.Add(row);
+            row = new string[] { "4", "Product 4", "4000" };
+            dgvWorkUnits.Rows.Add(row);
+
+          
+
+
+        }
     }
 }
