4d1eb0e803ad555e12b360e1310096273c62045d
dita9488 <dita9488@gmail.com>
2019-05-10 17:52:41 +0200

    a

--- Tick/TimeManagement/TimeCharts.Designer.cs
+++ Tick/TimeManagement/TimeCharts.Designer.cs
@@ -28,14 +28,48 @@
         /// </summary>
         private void InitializeComponent()
         {
+            this.chartControl1 = new Syncfusion.Windows.Forms.Chart.ChartControl();
             ((System.ComponentModel.ISupportInitialize)(this)).BeginInit();
             this.SuspendLayout();
+            // 
+            // chartControl1
+            // 
+            this.chartControl1.BackInterior = new Syncfusion.Drawing.BrushInfo(System.Drawing.Color.FromArgb(((int)(((byte)(44)))), ((int)(((byte)(42)))), ((int)(((byte)(50))))));
+            this.chartControl1.ChartArea.CursorLocation = new System.Drawing.Point(0, 0);
+            this.chartControl1.ChartArea.CursorReDraw = false;
+            this.chartControl1.ChartInterior = new Syncfusion.Drawing.BrushInfo(System.Drawing.Color.FromArgb(((int)(((byte)(44)))), ((int)(((byte)(42)))), ((int)(((byte)(50))))));
+            this.chartControl1.IsWindowLess = false;
+            // 
+            // 
+            // 
+            this.chartControl1.Legend.Location = new System.Drawing.Point(291, 75);
+            this.chartControl1.Localize = null;
+            this.chartControl1.Location = new System.Drawing.Point(64, 113);
+            this.chartControl1.Name = "chartControl1";
+            this.chartControl1.Palette = Syncfusion.Windows.Forms.Chart.ChartColorPalette.WarmCold;
+            this.chartControl1.PrimaryXAxis.LogLabelsDisplayMode = Syncfusion.Windows.Forms.Chart.LogLabelsDisplayMode.Default;
+            this.chartControl1.PrimaryXAxis.Margin = true;
+            this.chartControl1.PrimaryYAxis.LogLabelsDisplayMode = Syncfusion.Windows.Forms.Chart.LogLabelsDisplayMode.Default;
+            this.chartControl1.PrimaryYAxis.Margin = true;
+            this.chartControl1.Size = new System.Drawing.Size(400, 300);
+            this.chartControl1.TabIndex = 0;
+            this.chartControl1.Text = "chartControl1";
+            // 
+            // 
+            // 
+            this.chartControl1.Title.Name = "Default";
+            this.chartControl1.Titles.Add(this.chartControl1.Title);
+            this.chartControl1.VisualTheme = "";
             // 
             // TimeCharts
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(565, 395);
+            this.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(44)))), ((int)(((byte)(42)))), ((int)(((byte)(50)))));
+            this.ClientSize = new System.Drawing.Size(573, 425);
+            this.Controls.Add(this.chartControl1);
+            this.ForeColor = System.Drawing.Color.FromArgb(((int)(((byte)(161)))), ((int)(((byte)(161)))), ((int)(((byte)(161)))));
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.None;
             this.Name = "TimeCharts";
             // 
             // 
@@ -48,5 +82,7 @@
         }
 
         #endregion
+
+        private Syncfusion.Windows.Forms.Chart.ChartControl chartControl1;
     }
 }
